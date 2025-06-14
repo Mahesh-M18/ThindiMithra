174284368661eba47e15e350d64edc2a0995484e
Mahesh-M18 <123378752+Mahesh-M18@users.noreply.github.com>
2025-06-14 23:45:09 +0530

    Update settings.py

--- tindimithra/settings.py
+++ tindimithra/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-default-secret-k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-ALLOWED_HOSTS = ['thindimithra-u2o6.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = thindimithraa.onrender.com,localhost,127.0.0.1
+
 
 
 # Application definition
