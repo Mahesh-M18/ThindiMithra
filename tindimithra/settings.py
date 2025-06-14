83864a8d314556fbf1a4c92647f4446664c8f14e
Mahesh-M18 <123378752+Mahesh-M18@users.noreply.github.com>
2025-06-14 23:30:53 +0530

    Update settings.py

--- tindimithra/settings.py
+++ tindimithra/settings.py
@@ -28,6 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-default-secret-k
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['thindimithra-u2o6.onrender.com', 'localhost', '127.0.0.1']
+
 
 # Application definition
 INSTALLED_APPS = [
